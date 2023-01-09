1ab0521a73a71d1c16eb86bea3d613a1facb1715
Lalit K <lalitkumaran1602@gmail.com>
2023-01-09 23:53:40 +0530

    Designs commited

---
+++ Dev/server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+//const adminRouter = require("./routes/admin.js");
+//const authRouter = require("./routes/auth.js");
+//const busRouter = require("./routes/bus");
+//const userRouter = require("./routes/user");
+
+const DB="mongodb+srv://Lalit:Lalit123@cluster0.ktsia7a.mongodb.net/?retryWrites=true&w=majority"
+const PORT = 8000;
+const app=express();
+
+app.use(express.json());
+//app.use(authRouter);
+//app.use(adminRouter);
+//app.use(busRouter);
+//app.use(userRouter);
+
+mongoose.connect(DB).then( ()=> {
+    console.log("Connected successfully");
+}).catch( (e) =>{
+    console.log(e)
+});
+
+app.listen(PORT,'0.0.0.0',() => {
+    console.log(`connected at port ${PORT}`);
+});
